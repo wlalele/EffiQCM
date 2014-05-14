0f379cd341799efdbf5bb4e7b75655bd153ef70d
Mickael Blondeau <mickael.blondeau59@gmail.com>
2014-05-14 14:21:06 +0200

    Creation EffiQCMBundle

---
+++ src/Effi/QCMBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Effi\QCMBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('EffiQCMBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Effi/QCMBundle/Entity/Choice.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Effi\QCMBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Choice
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Choice
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
