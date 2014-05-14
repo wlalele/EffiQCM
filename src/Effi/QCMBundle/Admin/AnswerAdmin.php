9ea1d90ca3d00dabee304324495d8118951598e6
wlalele <amine.dai@free.fr>
2014-05-14 16:59:32 +0200

    Add Answer entity in admin panel

---
+++ src/Effi/QCMBundle/Admin/AnswerAdmin.php
@@ -0,0 +1,46 @@
+<?php
+// src/Effi/QCMBundle/Admin/AnswerAdmin.php
+
+namespace Effi\QCMBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class AnswerAdmin extends Admin
+{
+    // FORMULAIRE EDITION AJOUT
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('author', 'entity', array('class' => 'Effi\UserBundle\Entity\User'))
+            ->add('label', 'text')
+            ->add('isValid', 'checkbox', array('required' => false))
+            ->add('question', 'entity', array('class' => 'Effi\QCMBundle\Entity\Question'))
+        ;
+    }
+
+    // FORMULAIRE FILTRE
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('label')
+            ->add('author')
+            ->add('isValid')
+            ->add('question')
+        ;
+    }
+
+    // FORMULAIRE LISTE
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('label')
+            ->add('isValid')
+            ->add('question')
+            ->add('author')
+        ;
+    }
+}
